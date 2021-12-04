3e4899f30985a5a45f34a24529b6103f8f4054a0
edvan1997 <edvinci0000@gmail.com>
2021-12-03 21:55:56 -0300

    Atualizado

---
+++ index.php
@@ -0,0 +1,39 @@
+
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Aula 02</title>
+</head>
+<body>
+    <div class="main">
+       
+            <form >
+                <div class="campo">
+                    <label><strong>Usuário</strong></label>
+                    <input type="text" id="usuario" name="usuario" placeholder="Usuário" required>
+                    <div class="underline">
+                    </div>
+                </div>  
+                <div class="campo">
+                    <label><strong>Senha</strong></label>
+                    <input type="password" id="senha" name="senha" placeholder="Senha" required>
+                    <div class="underline">
+                    </div>
+                </div> 
+                <div class="login">
+                    <input type="submit" id="login" name="login" value="Login">
+                </div>
+            </form>  
+        
+    </div>
+    <script type="text/javascript" src="js/javascript.js"></script>
+</body>
+</html>
